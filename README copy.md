e84d949b380f58a14d8c030c50dbf980b84a093e
Alexander Samak <voprosy262@gmail.com>
2024-08-11 21:07:37 +0300

    feat: add gh-pages deploy

--- README copy.md
+++
@@ -1,1 +0,0 @@
-# youpland-frontend
--- src/lib/services/index.ts
+++
@@ -1,5 +0,0 @@
-import { UserService } from './user.service';
-
-const serverURL: string = import.meta.env.VITE_SERVER_URL;
-
-export const userService = new UserService(serverURL);
